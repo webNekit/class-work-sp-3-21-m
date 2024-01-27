72806722c3d381704b4ebe22fa9872898f7fa6fd
webNekit <mochalov-hunter@mail.ru>
2024-01-27 16:18:49 +0300

    Написали базовую функцию на цикле FOR

---
+++ task-5/app.js
@@ -0,0 +1,7 @@
+// Task - 5
+const peoples = ["Tom", "Vova", "Sam", "Bob", "Nat", "Ivan", "Stas"];
+console.log(peoples);
+for(let i = 0; i < peoples.length; i++) {
+    let result = peoples[i];
+    console.log(result);
+}
